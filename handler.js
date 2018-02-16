392ecced4cb65bbfb83a9369aa248da5d669a22d
Gonzalo Bahamondez <gonzalo.bahamondez.c@gmail.com>
2018-02-16 16:01:00 -0300

    Adding moment to test size --skip-ci

--- handler.js
+++ handler.js
@@ -1,12 +1,14 @@
 import someAsync from './some-async';
+import moment from 'moment';
 
 export async function hello (event, context, callback)  {  
   const result = await someAsync();  
+  const now = moment();
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: result,
+      input: now.toDate(),
     }),
   };
 
