a121f4edd6f1b55763c644401bb4e59b5a731112
Gonzalo Bahamondez <gonzalo.bahamondez.c@gmail.com>
2018-02-16 15:48:20 -0300

    Adding circle ci config --skip-ci

--- handler.js
+++ handler.js
@@ -1,14 +1,12 @@
 import someAsync from './some-async';
 
 export async function hello (event, context, callback)  {  
-
-  const result = await Promise.resolve(100);
-  console.info('result', result);
+  const result = await someAsync();  
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      input: result,
     }),
   };
 
